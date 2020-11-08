bc460ad157312f11b8ca0670a06276f17e237f54
ledutu <ledutu@gmail.com>
2020-11-08 22:54:16 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('furniture/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('furniture/css/bootstrap-grid.min.css')}}">
+    <link rel="stylesheet" href="{{asset('furniture/css/bootstrap-reboot.min.css')}}">
+    
+    <link rel='stylesheet' href='{{asset('assets/css/styles.min.css')}}'
+        type='text/css' />
+    <link rel='stylesheet' href='{{asset('assets/css/sweetalert.css')}}' type='text/css'/>
+    <link rel='stylesheet' href='{{asset('assets/css/sweetalert.css')}}' type='text/css'/>
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <div class="top">
+            <nav class="navbar navbar-expand-sm navbar-light bg-light">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="collapsibleNavId">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                            <div class="dropdown-menu" aria-labelledby="dropdownId">
+                                <a class="dropdown-item" href="#">Action 1</a>
+                                <a class="dropdown-item" href="#">Action 2</a>
+                            </div>
+                        </li>
+                    </ul>
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+            </nav>
+        </div>
+        <div class="bottom">
+            <div class="wrap-main d-flex al-center">
+                <div id="nav-icon">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+
+                <a href="https://thaicong.com/vi/" class="logo">
+                    
+                </a>
+
+                <div class="menu d-flex jc-between al-center">
+
+                    <!-- main menu -->
+                    <ul class="">
+                        <li class="nav-menu-item-225 ">
+                            <a class="" href="#">GIỚI THIỆU</a>
+                            <div class="sub">
+                                <div class="ctt-sub">
+                                    <div class="wrap-main">
+                                        <div class="row listRow">
+                                            <div class="col-xs-3">
+                                                <a href="#" class="">
+                                                    <span class="img"
+                                                        style="background-image: url(https://thaicong.com/wp-content/uploads/cache/2020/06/433x200/QTC-868x400-5.jpg)"></span>
+                                                    <span>QUÁCH THÁI CÔNG</span>
+                                                </a>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="nav-menu-item-226 ">
+                            <a class="" href="#">CỬA HÀNG</a>
+                            <div class="sub">
+                                <div class="ctt-sub">
+                                    <div class="wrap-main">
+                                        <div class="row listRow">
+                                            <div class="col-xs-3">
+                                                <a href="#" class="">
+                                                    <span class="img"
+                                                        style="background-image: url(https://thaicong.com/wp-content/uploads/cache/2017/12/433x200/img_sub_3.jpg)"></span>
+                                                    <span>SÀI GÒN – VIỆT NAM</span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="nav-menu-item-227 ">
+                            <a class="" href="#">DỊCH VỤ</a>
+                            <div class="sub">
+                                <div class="ctt-sub">
+                                    <div class="wrap-main">
+                                        <div class="row listRow">
+                                            <div class="col-xs-3">
+                                                <a href="#" class="">
+                                                    <span class="img"
+                                                        style="background-image: url(https://thaicong.com/wp-content/uploads/cache/2020/08/433x200/Tk-Noi-That-31.8.jpg)"></span>
+                                                    <span>THIẾT KẾ NỘI THẤT</span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="nav-menu-item-active">
+                            <a href="#" class="">SẢN PHẨM</a>
+                            <div class="sub">
+                                <div class="ctt-sub sub-product">
+                                    <div class="wrap-main">
+                                        <div class="item-sub">
+                                            <ul>
+                                                <li>
+                                                    <a href="#"><span
+                                                            class="ic-arrow-right-white"></span> Phòng khách</a>
+                                                    <div class="sub-list">
+                                                        <ul>
+                                                            <li><a
+                                                                    href="#">Sofa</a>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    
+                                        <div class="item-sub view-all">
+                                            <ul>
+                                                <li><a href="#" class="">XEM TẤT CẢ SẢN
+                                                        PHẨM</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="nav-menu-item-4546 ">
+                            <a class="" href="#">CÔNG TRÌNH</a>
+                            <div class="sub">
+                                <div class="ctt-sub">
+                                    <div class="wrap-main">
+                                        <div class="row listRow">
+                                            <div class="col-xs-3">
+                                                <a href="#"
+                                                    class="">
+                                                    <span class="img"
+                                                        style="background-image: url(https://thaicong.com/wp-content/uploads/cache/2020/06/433x200/congtrinh6.jpg)"></span>
+                                                    <span>CÔNG TRÌNH</span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="nav-menu-item-229 ">
+                            <a href="#">LIÊN HỆ</a>
+                        </li>
+                    </ul>
+
+                    <div>
+                        <a href="#" class="btn-page btn-2"><span
+                                class="ic-store mr5"></span><span>ONLINE STORE</span></a>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </header>
+    @for($i = 0; $i <= 2; $i++)
+    <footer>
+        <div class="top">
+            <div class="wrap-main">
+                <div class="row listRow">
+                    <div class="col-xs-2">
+                        <ul>
+                            <li><a href="https://thaicong.com/vi/gioi-thieu/">GIỚI THIỆU</a></li>
+                            <li><a href="https://thaicong.com/vi/cua-hang/">CỬA HÀNG</a></li>
+                            <li><a href="https://thaicong.com/vi/dich-vu/">DỊCH VỤ</a></li>
+                            <li><a href="https://thaicong.com/vi/san-pham/">SẢN PHẨM</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-2">
+                        <ul>
+                            <li><a href="https://thaicong.com/vi/phong-cach/">PHONG CÁCH</a></li>
+                            <li><a href="https://thaicong.com/vi/nha-hang/">NHÀ HÀNG</a></li>
+                            <li><a href="https://thaicong.com/vi/tuyen-dung/">TUYỂN DỤNG</a></li>
+                            <li><a href="https://thaicong.com/vi/lien-he/">LIÊN HỆ</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-2">
+                        <ul>
+                            <li><a href="https://thaicong.com/vi/cua-hang/hamburg-duc/">HAMBURG &#8211; ĐỨC</a></li>
+                            <li>
+                                <p>0049.40490.3311<br />Eppendorfer Weg 165, 20253,<br />HamBurg, Đức</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-2">
+                        <ul>
+                            <li><a href="https://thaicong.com/vi/cua-hang/sai-gon-viet-nam/">SÀI GÒN &#8211; VIỆT
+                                    NAM</a></li>
+                            <li>
+                                <p>028.38.989.818<br />215 A1 Nguyễn Văn Hưởng,<br />P. Thảo Điền, Q.2, Việt Nam</p>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="col-xs-4">
+                        <ul>
+                            <li><a href="">nhận bản tin</a></li>
+                            <li>
+                                <div class="form-group">
+                                    <input type="text" class="form-control" id="newsletter__email"
+                                        placeholder="Vui lòng nhập email của bạn và ENTER">
+                                    <a href="#" class="ic-search-arrow" id="btn__submit__newsletter"></a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bottom">
+            <div class="wrap-main d-flex jc-between al-center">
+                <div class="copy">
+                    <span>Copyright © 2017 Thai Cong Interior Design. All Rights Reserved.</span>
+                    <a href="#">Policy</a>
+                    <span>|</span>
+                    <a href="#">Privacy</a>
+                    <span>|</span>
+                    <a href="https://thaicong.com/vi/so-do-trang/" title="Sitemap">Sitemap</a>
+                </div>
+                <div class="share d-flex al-center">
+                    <span>Follow Us on</span>
+                    <a href="https://www.facebook.com/ThaiCongInteriorDesign/" target="_blank" class="ic-fb"></a>
+                    <a href="https://www.instagram.com/thaicong_official" target="_blank" class="ic-insta ml10"></a>
+                    <a href="https://www.youtube.com/channel/UC__Mf4U3476dWy5XATva3-g" target="_blank"
+                        class="ic-ytb ml10"></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+    @endfor
+    
+
+</body>
+
+<script src="{{asset('furniture/js/jquery.min.js')}}"></script>
+<script src="{{asset('furniture/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('furniture/js/bootstrap.min.js')}}"></script>
+
+</html>
